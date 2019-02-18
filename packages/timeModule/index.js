61a861e2e42a11b32a05a3518b5b150f711a9dd2
Rob Tarr <Rob@robtarr.net>
2019-02-18 15:41:01 -0500

    working without lerna

--- packages/timeModule/index.js
+++ packages/timeModule/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 
-const TimeModule = () => <h1>{moment.now().format('MMMM Do YYYY, h:mm:ss a')}</h1>
+const TimeModule = () => <h1>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h1>
 
 export default TimeModule;
